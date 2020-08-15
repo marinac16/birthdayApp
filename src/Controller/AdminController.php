500140926fa76c4bd124e365d44238116deebb61
Marina <cormerais10@gmail.com>
2020-08-15 21:04:01 +0200

    controller decomment

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -11,37 +11,37 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-//    /** @var DestinationRepository */
-//    private $destinationRepo;
-//
-//    /** @var VoteRepository */
-//    private $voteRepo;
-//
-//    /** @var CalculDesVotes */
-//    private $calculDesVotes;
-//
-//    public function __construct(DestinationRepository $destinationRepo, VoteRepository $voteRepo, CalculDesVotes $calculDesVotes)
-//    {
-//        $this->destinationRepo = $destinationRepo;
-//        $this->voteRepo = $voteRepo;
-//        $this->calculDesVotes = $calculDesVotes;
-//    }
-//    /**
-//     * @Route("/admin", name="admin")
-//     */
-//    public function index()
-//    {
-//
-//        $destinations = $this->destinationRepo->findAll();
-//
-//
-//
-//
-//
-//
-//        return $this->render('admin/index.html.twig', [
-//            "votes" => $this->calculDesVotes->calculerLesVotes(),
-//            "destinations" => $destinations,
-//        ]);
-//    }
+    /** @var DestinationRepository */
+    private $destinationRepo;
+
+    /** @var VoteRepository */
+    private $voteRepo;
+
+    /** @var CalculDesVotes */
+    private $calculDesVotes;
+
+    public function __construct(DestinationRepository $destinationRepo, VoteRepository $voteRepo, CalculDesVotes $calculDesVotes)
+    {
+        $this->destinationRepo = $destinationRepo;
+        $this->voteRepo = $voteRepo;
+        $this->calculDesVotes = $calculDesVotes;
+    }
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index()
+    {
+
+        $destinations = $this->destinationRepo->findAll();
+
+
+
+
+
+
+        return $this->render('admin/index.html.twig', [
+            "votes" => $this->calculDesVotes->calculerLesVotes(),
+            "destinations" => $destinations,
+        ]);
+    }
 }
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,131 +16,131 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class DefaultController extends AbstractController
 {
-//    /** @var DestinationRepository */
-//    private $destinationRepo;
-//
-//    public function __construct(DestinationRepository $destinationRepo)
-//    {
-//        $this->destinationRepo = $destinationRepo;
-//    }
-//
-//    /**
-//     * @Route("/", name="index")
-//     */
-//    public function index()
-//    {
-//
-//        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
-//
-//        $user = $this->getUser();
-//
-//        return $this->render('default/index.html.twig', [
-//            'user' => $user,
-//
-//        ]);
-//    }
-//
-//    /**
-//     * @Route("/choisi-la-destination", name="choix")
-//     * @param Request $request
-//     * @param EntityManagerInterface $em
-//     * @return Response
-//     */
-//    public function AfficherChoix(Request $request, EntityManagerInterface $em)
-//    {
-//        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
-//
-//        $vote = new Vote();
-//
-//        $user = $this->getUser();
-//
-//
-//        $form = $this->createForm(VoteType::class, $vote);
-//
-//        $form->handleRequest($request);
-//        if ($form->isSubmitted() && $form->isValid()) {
-//
-//            $vote->setUser($user);
-//
-//            $em->persist($vote);
-//            $em->flush();
-//
-//            if ($em)
-//
-//
-//            return $this->redirectToRoute('remerciement', []);
-//
-//        }
-//
-//        return $this->render('default/choix.html.twig', [
-//            'form'=> $form->createView(),
-//        ]);
-//
-//    }
-//
-//    /**
-//     * @Route("/remerciement", name="remerciement")
-//     */
-//    public function AfficherRemerciement()
-//    {
-//
-//        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
-//
-//        return $this->render('default/remerciement.html.twig', [
-//
-//        ]);
-//
-//    }
-//
-//    /**
-//     * @Route("/error", name="error")
-//     */
-//    public function AfficherError()
-//    {
-//
-//        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
-//
-//        return $this->render('default/error.html.twig', [
-//
-//        ]);
-//
-//    }
-//
-//    /**
-//     * @Route("/inscription", name="inscription")
-//     * @param Request $request
-//     * @param EntityManagerInterface $em
-//     * @param UserPasswordEncoderInterface $encoder
-//     * @return Response
-//     */
-//    public function Inscription(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder)
-//    {
-//
-//        $user = new User();
-//
-//        $userForm = $this->createForm(InscriptionType::class, $user);
-//        $userForm->handleRequest($request);
-//
-//        if ($userForm->isSubmitted() && $userForm->isValid()){
-//
-//            $hashed = $encoder->encodePassword($user, $user->getPassword());
-//            $user->setPassword($hashed);
-//
-//
-//
-//            $em->persist($user);
-//            $em->flush();
-//
-//            return $this->redirectToRoute('inscription');
-//        }
-//
-//
-//
-//        return $this->render('default/inscription.html.twig', [
-//            'form'=>$userForm->createView()
-//        ]);
-//
-//    }
+    /** @var DestinationRepository */
+    private $destinationRepo;
+
+    public function __construct(DestinationRepository $destinationRepo)
+    {
+        $this->destinationRepo = $destinationRepo;
+    }
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+        $user = $this->getUser();
+
+        return $this->render('default/index.html.twig', [
+            'user' => $user,
+
+        ]);
+    }
+
+    /**
+     * @Route("/choisi-la-destination", name="choix")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function AfficherChoix(Request $request, EntityManagerInterface $em)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+        $vote = new Vote();
+
+        $user = $this->getUser();
+
+
+        $form = $this->createForm(VoteType::class, $vote);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $vote->setUser($user);
+
+            $em->persist($vote);
+            $em->flush();
+
+            if ($em)
+
+
+            return $this->redirectToRoute('remerciement', []);
+
+        }
+
+        return $this->render('default/choix.html.twig', [
+            'form'=> $form->createView(),
+        ]);
+
+    }
+
+    /**
+     * @Route("/remerciement", name="remerciement")
+     */
+    public function AfficherRemerciement()
+    {
+
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+        return $this->render('default/remerciement.html.twig', [
+
+        ]);
+
+    }
+
+    /**
+     * @Route("/error", name="error")
+     */
+    public function AfficherError()
+    {
+
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+        return $this->render('default/error.html.twig', [
+
+        ]);
+
+    }
+
+    /**
+     * @Route("/inscription", name="inscription")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param UserPasswordEncoderInterface $encoder
+     * @return Response
+     */
+    public function Inscription(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder)
+    {
+
+        $user = new User();
+
+        $userForm = $this->createForm(InscriptionType::class, $user);
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()){
+
+            $hashed = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hashed);
+
+
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('inscription');
+        }
+
+
+
+        return $this->render('default/inscription.html.twig', [
+            'form'=>$userForm->createView()
+        ]);
+
+    }
 
 
 }
